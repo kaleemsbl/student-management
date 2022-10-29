9dfed12a3d8dc4599289b0537cf25346f7f1f3a1
Kaleem Khan <kaleemkhan05071995>
2022-10-29 09:52:27 +0530

    Axios Interceptor and EVN setup

---
+++ src/pages/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="q-pa-md">
+    <q-page class="flex flex-center">
+      <div class="q-gutter-y-md column login" style="width: 30%">
+
+        <q-input label="Email" hint="Enter Your Email" />
+
+        <q-input label="Password" type="password" hint="Password" />
+
+        <q-btn color="purple mt-50" label="Login" />
+      </div>
+    </q-page>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+      env: process.env.API
+    }
+  },
+  async created() {
+    let res = await this.$api.post('/auth/login', {
+      "email": "fake@example.com",
+      "password": "password1"
+    });
+
+    console.log(res)
+  }
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,25 @@
+import MainLayout from "layouts/MainLayout.vue";
+import LoginLayout from "layouts/LoginLayout.vue";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    path: "/",
+    component: MainLayout,
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+  },
+
+  {
+    path: "/login",
+    component: LoginLayout,
+    children: [{ path: "", component: () => import("pages/Login.vue") }],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
