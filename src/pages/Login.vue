49291b65dc3127c46e0defa29dce46c8578b0d47
Kaleem Khan <kaleemkhan05071995>
2022-10-30 11:16:10 +0530

    Login API integretion

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,12 +2,32 @@
   <div class="q-pa-md">
     <q-page class="flex flex-center">
       <div class="q-gutter-y-md column login" style="width: 30%">
+        <q-form @submit="submitLoginData">
 
-        <q-input label="Email" hint="Enter Your Email" />
+          <q-input
+            v-model="loginData.email"
+            :rules="[
+              (val) => (val && val != '') || 'This field is required!',
+              (val) => validateEmail(val) || 'Must be a valid email.',
+            ]" 
+            label="Email"
+            hint="Enter Your Email"
+            />
 
-        <q-input label="Password" type="password" hint="Password" />
+          <q-input
+            v-model="loginData.password"
+            :rules="[
+              (val) => (val && val != '') || 'This field is required!',
+            ]"
+            label="Password"
+            type="password"
+            hint="Password"
+            />
 
-        <q-btn color="purple mt-50" label="Login" />
+          <q-btn color="purple mt-50" label="Login" type="submit" />
+
+        </q-form>
+
       </div>
     </q-page>
   </div>
@@ -15,20 +35,34 @@
 </template>
 
 <script>
+import Cookies from 'js-cookie';
+import { Loading, Notify } from 'quasar'
 export default {
   name: 'LoginPage',
   data() {
     return {
-      env: process.env.API
+      loginData: {},
     }
   },
-  async created() {
-    let res = await this.$api.post('/auth/login', {
-      "email": "fake@example.com",
-      "password": "password1"
-    });
-
-    console.log(res)
+  methods: {
+    async submitLoginData() {
+      Loading.show();
+      try {
+        let result = await this.$api.post('/auth/login', this.loginData);
+        Cookies.set('Token', result.data.tokens.access.token);
+        this.$router.push('/');
+      } catch (error) {
+        Notify.create({
+          position: 'top-right',
+          type: 'negative',
+          message: error.response.data.message
+        })
+      }
+      Loading.hide();
+    },
+    validateEmail(email) {
+      return /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(email);
+    }
   }
 }
 </script>
